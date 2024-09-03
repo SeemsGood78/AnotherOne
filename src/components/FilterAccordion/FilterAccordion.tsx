eb96b4e31cda146c9664d5fca2c0c122810a4741
Pablo <SeemsGood783@gmail.com>
2024-09-03 22:35:38 +0300

    Crash

--- src/components/FilterAccordion/FilterAccordion.tsx
+++ src/components/FilterAccordion/FilterAccordion.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import style from './style.module.scss'
 import { useState } from 'react';
 import { useStore } from '../store';
+import ReactSlider from 'react-slider';
 
 const FilterAccordion = () => {
 
-    const {applyFilters} = useStore();
+    const { applyFilters } = useStore();
     const [openIds, setOpenIds] = useState<number[]>([]);
     const filter: Record<string, any> = {};
+    const [price, setPrice] = useState([0, 100]);
 
     const rule = {
         Volume: [0.3, 0.5, 0.8],
         Availability: ['In stock', 'Out of stock'],
-        Price: ['<20', '20-40', '40-80', '80>'],
+        Price: price,
         Type: ["Stout", "IPA", "Brown Ale", "Porter", "Saison", "Wheat Beer", "Pale Ale", "Sour Beer", "Lager", "Amber Ale"],
     };
 
@@ -29,6 +31,7 @@ const FilterAccordion = () => {
         const existValue = filter?.[par] ?? [];
         const newValue = existValue.includes(value) ? existValue.filter((item: any) => item != value) : [...existValue, value];
         filter[par] = newValue;
+        console.log(filter) // ??
     };
 
     return (
@@ -54,16 +57,34 @@ const FilterAccordion = () => {
                                 </span>
                             </div>
                             {openIds.includes(index) && (
-                                <ul className={style.filterAccordion_content} >
-                                    {value.map((value, index) => (
-                                        <li key={index}>
-                                            <label>
-                                                <input type="checkbox" value={value} onClick={(e) => checkValue(e, par)} />
-                                                <span>{value}</span>
-                                            </label>
-                                        </li>
-                                    ))}
-                                </ul>
+                                <>
+                                    {par === 'Price' ? (
+                                        <div>
+                                            <span>From {price[0]}$ to {price[1]}$</span>
+                                            <ReactSlider className={style.slider} thumbClassName={style.thumb}
+                                                onChange={(newValue) => {
+                                                    setPrice(newValue);
+                                                    console.log(newValue);
+                                                }}
+                                                value={price}
+                                                min={0}
+                                                max={100}
+                                                step={1}
+                                            />
+                                        </div>
+                                    ) : (
+                                        <ul className={style.filterAccordion_content}>
+                                            {value.map((value, index) => (
+                                                <li key={index}>
+                                                    <label>
+                                                        <input type="checkbox" value={value} onClick={(e) => checkValue(e, par)} />
+                                                        <span>{value}</span>
+                                                    </label>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </>
                             )}
                         </React.Fragment>
                     );
